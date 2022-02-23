099473c1de7efbcf712532cae8313bb5ac70fe2b
Idontgetit1 <thumbenjamin@gmail.com>
2022-02-23 22:03:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Svg;
+
+namespace svgzToPng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get all .svgz files in current directory
+            string[] svgzFiles = System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), "*.svgz");
+
+            Program.transformSVGZToPNG(svgzFiles);
+        }
+
+        public static void transformSVGZToPNG(string[] paths){
+            foreach (string path in paths){
+                // load svg file as MemoryStream
+                SvgDocument svgDoc = SvgDocument.Open(path);
+                Bitmap bitmap = svgDoc.Draw();
+                bitmap.Save(path.Replace(".svgz", ".png"), ImageFormat.Png);
+            }
+        }
+    }
+}
